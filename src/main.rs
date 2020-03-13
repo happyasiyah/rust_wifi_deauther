289099b28edd6551a46f43264e6e8c70654c2f39
markrp <markrepedersen@gmail.com>
2020-03-12 22:34:30 -0700

    cli args

--- src/main.rs
+++ src/main.rs
@@ -2,19 +2,58 @@ mod frame;
 mod packet;
 mod parse;
 
+use custom_debug_derive::*;
+use clap::{Arg, App};
 use frame::{Frame};
-use pnet::datalink::{self, Channel::Ethernet, MacAddr};
-use pnet::packet::ethernet::{MutableEthernetPacket};
+use pnet::datalink::{self, channel, Channel::Ethernet, MacAddr};
+use pnet::packet::ethernet::{MutableEthernetPacket, EtherType};
+
+#[derive(CustomDebug)]
+struct CLI {
+    verbose: bool,
+    interface: String,
+}
+
+impl CLI {
+    fn new() -> Self {
+	let matches = App::new("rust_wifi_deauther")
+            .version("1.0")
+            .author("Mark Pedersen <markrepedersen@gmail.com>")
+            .about("Send deauth frames to all devices on subnet.")
+            .arg(Arg::with_name("interface")
+		 .short("i")
+		 .long("interface")
+		 .value_name("name")
+		 .help("The name of the wireless network interface. On MacOS, the wifi interface is 'en0'.")
+		 .takes_value(true)
+		 .default_value("en0")
+		 .required(true))
+	    .arg(Arg::with_name("verbose")
+		 .short("v")
+		 .long("verbose")
+		 .help("Enable verbose mode."))
+	    .get_matches();
+	let interface = matches
+	    .value_of("interface")
+	    .expect("Network interface parameter is required.");
+	let verbose = matches.is_present("verbose");
+	CLI {
+	    interface: String::from(interface),
+	    verbose: verbose
+	}
+    }
+}
 
 fn main() {
+    let args = CLI::new();
     let interfaces = datalink::interfaces();
     let interface = interfaces
 	.into_iter()
-	.filter(|iface| iface.name == "en0")
+	.filter(|iface| iface.name == args.interface)
 	.next()
 	.unwrap();
 
-    let (mut tx, mut rx) = match datalink::channel(&interface, Default::default()) {
+    let (mut tx, mut rx) = match channel(&interface, Default::default()) {
 	Ok(Ethernet(tx, rx)) => (tx, rx),
 	Ok(_) => panic!("Unhandled channel type"),
 	Err(e) => panic!("Datalink channel error: {}", e),
@@ -24,27 +63,31 @@ fn main() {
 	match rx.next() {
 	    Ok(packet) => {
 		let frame: Frame = process_packet(packet);
-		tx.build_and_send(1, packet.len(), &mut |new_packet| {
-		    let mut new_packet: MutableEthernetPacket = MutableEthernetPacket::new(new_packet).unwrap();
-		    let source = frame.src.0;
-		    let dest = frame.dst.0;
-		    new_packet.set_ethertype(pnet::packet::ethernet::EtherType::new(3));
-                    new_packet.set_source(MacAddr::new(
-			source[0],
-			source[1],
-			source[2],
-			source[3],
-			source[4],
-			source[5]));
-                    new_packet.set_destination(MacAddr::new(
-			dest[0],
-			dest[1],
-			dest[2],
-			dest[3],
-			dest[4],
-			dest[5]));
-		});
-		panic!();
+		println!("{:?}", args);
+		if args.verbose {
+		    println!("{:#?}", frame);   
+		}
+		// tx.build_and_send(1, packet.len(), &mut |new_packet| {
+		//     let mut new_packet: MutableEthernetPacket = MutableEthernetPacket::new(new_packet).unwrap();
+		//     let source = frame.src.0;
+		//     let dest = frame.dst.0;
+
+		//     new_packet.set_ethertype(EtherType::new(3));
+                //     new_packet.set_source(MacAddr::new(
+		// 	source[0],
+		// 	source[1],
+		// 	source[2],
+		// 	source[3],
+		// 	source[4],
+		// 	source[5]));
+                //     new_packet.set_destination(MacAddr::new(
+		// 	dest[0],
+		// 	dest[1],
+		// 	dest[2],
+		// 	dest[3],
+		// 	dest[4],
+		// 	dest[5]));
+		// });
 	    }
 	    Err(e) => {
 		panic!("An error occurred while reading packet: {}", e);
@@ -56,9 +99,6 @@ fn main() {
 fn process_packet(packet: &[u8]) -> frame::Frame {
     match frame::Frame::parse(packet) {
 	Ok((_remaining, frame)) => {
-	    // if let Some(frame::EtherType::IPv4) = frame.ether_type {
-	    // 	println!("{:#?}", frame);
-	    // }
 	    return frame;
 	}
 	Err(nom::Err::Error(e)) => {
